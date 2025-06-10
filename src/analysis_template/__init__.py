d6cb951ad04beb35b27083d54c6d5ca301de5fb8
Eddie Mattia <eddie@outerbounds.com>
2025-06-10 15:28:09 -0700

    fix: Harden pyproject.toml update logic

--- src/analysis_template/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib import metadata
-
-try:
-    __version__: str = metadata.version("analysis-template")
-except metadata.PackageNotFoundError:
-    __version__ = "0.0.0"
-
-__all__ = ["__version__"]
